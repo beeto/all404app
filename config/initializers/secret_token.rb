73b646ad87bc3f0c4dcb980409a445fd0e62ccb9
David Beatty <db@davidbeatty.com>
2013-08-28 11:28:54 +0800

    first commit - empty rails app with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+All404app::Application.config.secret_token = '1463d100ebf1ad1787726d5359d71f7cd7490915a79a688cf3e1f283c946186b606b6637939479b5536dbd8a6cfae3f014587185de5db2e0b8ddcd4ec932cc74'
